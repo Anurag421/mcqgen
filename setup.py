d3e908ff8d46425d9f88f2c54ce1f072144dd918
Anurag421 <anuragsinghaakash@gmail.com>
2024-06-19 18:07:49 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import find_packages, setup
+
 setup(
     name='mcqgenerator',
     version='0.0.1',
     author='anuragsinghaakash',
     author_email="anuragsinghaakash@gmail.com",
-    install_requires=["openai", "langchain","streamlet",'python-dotenv','PyPDF2'],
+    install_requires=["openai",
+                      "langchain",
+                      "streamlit",
+                      "python-dotenv",
+                      "PyPDF2",
+                      "langchain_community"],
     packages=find_packages()
 )
