9b215b3b8c0d81d68d5ffefa5f31293076505cc4
Anurag421 <anuragsinghaakash@gmail.com>
2024-06-26 18:24:59 +0530

    imported required libraries with in streamlit file

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -2,15 +2,11 @@ import os
 import json
 import pandas as pd
 import traceback
-import PyPDF2
+ 
 from dotenv import load_dotenv
 from src.mcqgen.utils import read_file, get_table_data
 from src.mcqgen.logger import logging
 import streamlit as st
 
-#importing required libraries for longchain
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.callbacks import get_openai_callback
-from langchain.chains import SequentialChain
+# importing required libraries for longchain
+from src.mcqgen.mcqgenerator import generate_evaluate_chain
