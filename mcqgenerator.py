92a52f1fffbfeb946391ae96f1ee3181ec562c28
Anurag Singh Aakash <86045346+Anurag421@users.noreply.github.com>
2024-06-26 11:48:08 +0530

    added libraies and openai key

--- mcqgenerator.py
+++ mcqgenerator.py
@@ -2,8 +2,11 @@ import os
 import json
 import pandas as pd
 import traceback
+from dotenv import load_dotenv
+from src.mcqgen.utils import read_file, get_table_data
+from src.mcqgen.logger import logging
 
-
+#importing required libraries for longchain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -11,3 +14,6 @@ from langchain.callbacks import get_openai_callback
 from langchain.chains import SequentialChain
 import PyPDF2
 
+#load environment variable from dotenv
+load_dotenv()
+os.getenv("OPENAI_API_KEY")
